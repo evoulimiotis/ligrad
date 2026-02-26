4b93ded133589406730100a47ae046944a1f9b7e
evoulimiotis <evoulimiotis2003@gmail.com>
2026-02-26 16:07:41 +0200

    Updated version to 1.1.1 in __init__.py

--- ligrad/__init__.py
+++ ligrad/__init__.py
@@ -4,4 +4,4 @@ rotated_spheroid, vis_mask, ellipse_radius, gravity_darkening, limb_darkening, b
 # __all__ = ["grav_dark_transit_model","spheroid","rotation_matrix_x","rotation_matrix_y","rotation_matrix_z",
 #            "rotated_spheroid","vis_mask","ellipse_radius","gravity_darkening","limb_darkening","baseline_flux",
 #            "planet_integration","planet_position"]
-# __version__ = "1.1.0"
+# __version__ = "1.1.1"

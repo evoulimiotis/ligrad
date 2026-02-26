a5fa48b32da87deae197434079d2a39e67262ad5
evoulimiotis <evoulimiotis2003@gmail.com>
2026-02-26 04:03:45 +0200

    Bump version to 1.1.0 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='ligrad',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(),
     install_requires=[
         "numpy>=1.17",

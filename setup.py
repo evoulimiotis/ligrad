ca2ebbc6f1d144beb80cbaf6c742e113364058ad
evoulimiotis <evoulimiotis2003@gmail.com>
2026-03-09 01:04:53 +0200

    Updated version to 1.1.2 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='ligrad',
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(),
     install_requires=[
         "numpy>=1.17",

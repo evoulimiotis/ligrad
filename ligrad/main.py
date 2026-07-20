42326241c68b48c6c94695204505b952ba8233ed
evoulimiotis <evoulimiotis2003@gmail.com>
2026-07-20 04:01:34 +0300

    Added new function vsini2omega and optimized the occulted flux calculation. Updated some of the function names.

--- ligrad/main.py
+++ ligrad/main.py
@@ -3,6 +3,8 @@ from scipy.spatial import ConvexHull, cKDTree
 from astropy import constants as const
 import pylightcurve as plc
 
+
+
 Ms = const.M_sun.value
 Ls = const.L_sun.value
 Rs = const.R_sun.value
@@ -10,6 +12,8 @@ G = const.G.value
 h = const.h.value
 c = const.c.value
 k_B = const.k_B.value
+
+
 
 def spheroid(Re_eq, Rp_polar, n):
     i = np.arange(n)
@@ -22,20 +26,28 @@ def spheroid(Re_eq, Rp_polar, n):
     z = r*np.cos(lat)
     return x, y, z, lat
 
+
+
 def rotation_matrix_x(angle):
     return np.array([[1, 0, 0],
                      [0, np.cos(angle), -np.sin(angle)],
                      [0, np.sin(angle),  np.cos(angle)]])
 
+
+
 def rotation_matrix_y(angle):
     return np.array([[ np.cos(angle), 0, np.sin(angle)],
                      [0, 1, 0],
                      [-np.sin(angle), 0, np.cos(angle)]])
-    
+
+
+
 def rotation_matrix_z(angle):
     return np.array([[np.cos(angle), -np.sin(angle), 0],
                     [np.sin(angle),  np.cos(angle), 0],
                     [0, 0, 1]])
+
+
 
 def rotated_spheroid(x, y, z, lamda, i_s):
     p = np.array([x.flatten(), y.flatten(), z.flatten()])
@@ -44,6 +56,8 @@ def rotated_spheroid(x, y, z, lamda, i_s):
     x_rot, y_rot, z_rot = rot_p[0], rot_p[1], rot_p[2]
     return x_rot, y_rot, z_rot, R
 
+
+
 def vis_mask(x, y, z, Re_eq, Rp_polar, R):
     Nx = x/Re_eq**2
     Ny = y/Re_eq**2
@@ -51,6 +65,8 @@ def vis_mask(x, y, z, Re_eq, Rp_polar, R):
     norm = np.array([Nx, Ny, Nz])
     rot_norm = R @ norm #### rotate the normals
     return rot_norm[0] > 0  #### only the points with positive norm elements should be visible
+
+
 
 def ellipse_radius(y_vis, z_vis, theta):
     ell_pts = np.column_stack([y_vis, z_vis])
@@ -71,6 +87,8 @@ def ellipse_radius(y_vis, z_vis, theta):
     r = (a_s*b_s)/np.sqrt((b_s*np.cos(phi))**2 + (a_s*np.sin(phi))**2)  ##### radius of the projected ellipse at theta, centered around (yc, zc)
     return r, yc, zc
 
+
+
 def gravity_darkening(st_mass, st_mean_temperature, beta, omega, obs_wavelength, Re_eq, Rp_polar, theta):
     r = Re_eq*np.sqrt(1/(np.sin(theta)**2 + ((Re_eq/Rp_polar)*np.cos(theta))**2))
     Omega_Kepl = np.sqrt(G*st_mass/(Re_eq**3))
@@ -82,54 +100,79 @@ def gravity_darkening(st_mass, st_mean_temperature, beta, omega, obs_wavelength,
     I_planck = (2*h*c**2)/(obs_wavelength**5)/(np.exp(h*c/(obs_wavelength*k_B*temperature)) - 1)
     return I_planck
 
-def limb_darkening(y_proj, z_proj, yc, zc, r, u1, u2, u3, u4):
+
+
+def limb_darkening(y_proj, z_proj, yc, zc, r, u1, u2):
     y_cent = y_proj - yc
     z_cent = z_proj - zc
     rho = np.sqrt(y_cent**2 + z_cent**2)/r
     mu = np.sqrt(np.clip(1 - rho**2, 0, None))
     I_claret = np.ones_like(rho)
-    inside = rho <= 1  #####m using the 4-term Claret law for limb darkening calculation
-    I_claret[inside] = 1 - u1*(1 - (mu[inside])**(1/2)) - u2*(1 - mu[inside]) - u3*(1 - (mu[inside])**(3/2)) - u4*(1 - (mu[inside])**2)
+    inside = rho <= 1  #####m using the quadratic law for limb darkening calculation
+    I_claret[inside] = 1 - u1*(1 - mu[inside]) - u2*(1 - mu[inside])**2
     I_claret[~inside] = 0.0
     return I_claret
 
-def baseline_flux(I_total, y_vis, z_vis):
+
+
+def unocculted_flux(I_total, y_vis, z_vis):
     p = np.column_stack([y_vis, z_vis])
     hull = ConvexHull(p)
     proj_area = hull.volume  ##### projected area of the visible stellar disk
     dA = proj_area/len(I_total)  ### the area element for integration
     return np.sum(I_total)*dA
 
-def planet_integration(y_p, z_p, Rp_phys, I_total, yc, zc, ped, r_ell, tree, integration_grid_size=45):
-    if Rp_phys <= 0:
+
+
+def occulted_flux(y_occ, z_occ, Rocc_phys, I_total, yc, zc, ped, r_ell, tree, dy, dz, dA):
+    if Rocc_phys <= 0:
         return 0.0
-    n = integration_grid_size
-    y_grid = np.linspace(y_p - Rp_phys, y_p + Rp_phys, n)  ###### making a uniform grid over the planet disk
-    z_grid = np.linspace(z_p - Rp_phys, z_p + Rp_phys, n)
-    Y, Z = np.meshgrid(y_grid, z_grid)
-    Y, Z = Y.flatten(), Z.flatten()
-    planet = (Y - y_p)**2 + (Z - z_p)**2 <= Rp_phys**2  ##### mask for inside planet disk
-    Y, Z = Y[planet], Z[planet]
-    if len(Y) == 0:
-        return 0.0
+    
+    Y = y_occ + dy
+    Z = z_occ + dz
     point_ang = np.arctan2(Z - zc, Y - yc)  ## the polar angle of each planet grid point (Yi,Zi) measured from the stellar center
-    point_ang = np.where(point_ang < 0, point_ang + 2*np.pi, point_ang)
     r_at_points = np.interp(point_ang, ped, r_ell)
-    on_star = np.sqrt((Y - yc)**2 + (Z - zc)**2) <= r_at_points  #### mask for inside the stellar 2-d projected elliptic disk
-    Y, Z = Y[on_star], Z[on_star]
-    if len(Y) == 0:
+    on_star = ((Y - yc)**2 + (Z - zc)**2) <= r_at_points**2  #### mask for inside the stellar 2-d projected elliptic disk
+    
+    Y_final, Z_final = Y[on_star], Z[on_star]
+    
+    if len(Y_final) == 0:
         return 0.0
-    _, iss = tree.query(np.column_stack([Y, Z]), k=1)  ###### index of the nearest stellar point for each planet grid point
-    dA = (2*Rp_phys/n)**2
+    
+    _, iss = tree.query(np.column_stack([Y_final, Z_final]), k=1)  ###### index of the nearest stellar point for each occulted grid point
     return np.sum(I_total[iss])*dA
 
-def planet_position(t, period, e, inc, w, t_p, st_mass):
-        a = (G*st_mass*((period*86400)**2)/(4*np.pi**2))**(1/3)
-        return plc.planet_orbit(period, a, e, inc, w, t_p, t)
+
+
+def planet_position(t, orb_period, e, i_0, omega_p, raan, t_p, st_mass):
+        a = (G*st_mass*((orb_period*86400)**2)/(4*np.pi**2))**(1/3)
+        return plc.planet_orbit(orb_period, a, e, i_0, omega_p, t_p, t, raan)
+
+
+
+def vsini2omega(vsini_kms, st_mass_solar, R_mean_solar, i_s_deg):
+    st_mass_solar, R_mean_solar = st_mass_solar*Ms, R_mean_solar*Rs
+    
+    i_s = np.deg2rad(i_s_deg)
+    if np.abs(np.sin(i_s)) < 1e-9:
+        return np.inf
+    omega = 0.3  ### guess
+    for _ in range(100):
+        R_eq = R_mean_solar*(((2 + omega**2)/2)**(1/3))
+        v_eq = (vsini_kms*1e3)/np.sin(i_s)
+        Omega = v_eq/R_eq
+        Omega_crit = np.sqrt(G*st_mass_solar/(R_eq**3))
+        omega_new = Omega/Omega_crit
+        if np.abs(omega_new - omega) < 1e-9:
+            break
+        omega = omega_new
+    return omega_new
+
+
 
 def grav_dark_transit_model(t_vals, orbital_period, st_mass, st_mean_radius, st_mean_temperature, 
-                             beta, lamda, i_s, omega, u1, u2, u3, u4, e, i_0, omega_p, t_p, 
-                             rp_rs, obs_wavelength=800e-9, integration_grid_size=45):
+                             beta, lamda, i_s, omega, u1, u2, e, i_0, omega_p, raan, t_p, 
+                             rp_rs, obs_wavelength=800e-9, integration_grid_size=1):
     st_mass, st_mean_radius, st_mean_temperature = st_mass*Ms, st_mean_radius*Rs, st_mean_temperature*10000
     R_eq = st_mean_radius*((2 + omega**2)/2)**(1/3)  ##### for the volume-preserving stellar mean radius
     R_polar = R_eq*(2/(2 + omega**2))
@@ -145,17 +188,29 @@ def grav_dark_transit_model(t_vals, orbital_period, st_mass, st_mean_radius, st_
     point_angles = np.arctan2(z_vis - zc, y_vis - yc)
     point_angles = np.where(point_angles < 0, point_angles + 2*np.pi, point_angles)
     r_points = np.interp(point_angles, ped, r_ell)
-    I_limb = limb_darkening(y_vis, z_vis, yc, zc, r_points, u1, u2, u3, u4)
+    I_limb = limb_darkening(y_vis, z_vis, yc, zc, r_points, u1, u2)
     I_total = I_grav[mask]*I_limb    ###### computing the full intensity profile
     
-    F_out = baseline_flux(I_total, y_vis, z_vis)
+    F_out = unocculted_flux(I_total, y_vis, z_vis)
     tree = cKDTree(np.column_stack([y_vis, z_vis]))
     Rp_phys = rp_rs*st_mean_radius  ###### again for the volume-preserving mean radius
+    
+    integration_grid_size = 250*integration_grid_size
+    i = np.arange(integration_grid_size)
+    g_rat = (1 + np.sqrt(5))/2
+    r = Rp_phys*np.sqrt((i + 0.5)/integration_grid_size)
+    theta = 2*np.pi*i/(g_rat**2)  ###### making the uniform planet disk points using the Fibonacci sequence
+    planet_dy = r*np.cos(theta)
+    planet_dz = r*np.sin(theta)
+    dA = (np.pi*Rp_phys**2)/integration_grid_size
+    
+    R_star_max = np.max(r_ell)
     flux = []
     for t in t_vals:
-        x_p, y_p, z_p = planet_position(t, orbital_period, e, i_0, omega_p, t_p, st_mass)
-        if x_p >= 0:
-            DF = planet_integration(y_p, z_p, Rp_phys, I_total, yc, zc, ped, r_ell, tree, integration_grid_size)
+        x_p, y_p, z_p = planet_position(t, orbital_period, e, i_0, omega_p, raan, t_p, st_mass)
+        d_proj = np.sqrt((y_p - yc)**2 + (z_p - zc)**2)
+        if x_p >= 0 and d_proj <= (R_star_max + 1.05*Rp_phys):
+            DF = occulted_flux(y_p, z_p, Rp_phys, I_total, yc, zc, ped, r_ell, tree, planet_dy, planet_dz, dA)
             flux_norm = (F_out - DF)/F_out  #### calculating the normalized flux at each time step
         else:
             flux_norm = 1.0

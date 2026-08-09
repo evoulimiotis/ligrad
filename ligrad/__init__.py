6cea8622b858c2f735b49565de8cfe4a1a400382
evoulimiotis <evoulimiotis2003@gmail.com>
2026-08-09 23:08:45 +0300

    Minor fix

--- ligrad/__init__.py
+++ ligrad/__init__.py
@@ -5,4 +5,4 @@ vsini2omega
 # __all__ = ["grav_dark_transit_model","spheroid","rotation_matrix_x","rotation_matrix_y","rotation_matrix_z",
 #            "rotated_spheroid","vis_mask","ellipse_radius","gravity_darkening","limb_darkening","unocculted_flux",
 #            "occulted_flux","planet_position","vsini2omega"]
-# __version__ = "1.1.4"
+# __version__ = "1.1.5"

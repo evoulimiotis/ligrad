9c89d41a8f5262637042831fe965581a37659651
evoulimiotis <evoulimiotis2003@gmail.com>
2026-08-22 01:49:32 +0300

    Replaced planet_position with kep2car.

--- ligrad/__init__.py
+++ ligrad/__init__.py
@@ -1,5 +1,5 @@
 from .main import grav_dark_transit_model, spheroid, rotation_matrix_x, rotation_matrix_y, rotation_matrix_z, \
-rotated_spheroid, vis_mask, ellipse_radius, gravity_darkening, limb_darkening, unocculted_flux, occulted_flux, planet_position, \
+rotated_spheroid, vis_mask, ellipse_radius, gravity_darkening, limb_darkening, unocculted_flux, occulted_flux, kep2car, \
 vsini2omega
 
 # __all__ = ["grav_dark_transit_model","spheroid","rotation_matrix_x","rotation_matrix_y","rotation_matrix_z",

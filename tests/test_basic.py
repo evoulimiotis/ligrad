acdd927ce00bc25f02b362e3bf275d031d73936d
evoulimiotis <evoulimiotis2003@gmail.com>
2026-09-22 18:13:36 +0300

    Adjusted for new version

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,23 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from ligrad.main import grav_dark_transit_model, vsini2omega
+from ligrad.main import gd_transit_model, vsini2omega
 
 time = np.linspace(-0.1, 0.1, 320)
 
-flux = grav_dark_transit_model(t_vals = time, orbital_period = 5.0, st_mass = 1.0, st_mean_radius = 1.0, 
+flux = gd_transit_model(t_vals = time, orbital_period = 5.0, st_mass = 1.0, st_mean_radius = 1.0, 
                                st_mean_temperature = 0.58, beta = 0.2, lamda = 70.0, i_s = 50.0, omega = 0.6, 
                                u1 = 0.5, u2 = -0.1, e = 0.0, i_0 = 88.0, omega_p = 0.0, raan = 0.0, t_mid = 0.0, 
-                               rp_rs = 0.1, obs_wavelength=800e-9, integration_grid_size=2)
+                               rp_rs = 0.1, obs_wavelength=800e-9, pl_grid=2)
 
 ### in case you want to use real rotational velocities (v*sini in km/s) for the stellar rotation you can use the vsini2omega function,
 ### to calculate the true dimensionless rotation factor (omega). See the commented example below:
 
 
 # om = vsini2omega(vsini_kms = 195.4, st_mass_solar = 1.0, R_mean_solar = 1.0, i_s_deg = 50)
-# flux2 = grav_dark_transit_model(t_vals = time, orbital_period = 5.0, st_mass = 1.0, st_mean_radius = 1.0, 
+# flux2 = gd_transit_model(t_vals = time, orbital_period = 5.0, st_mass = 1.0, st_mean_radius = 1.0, 
 #                                st_mean_temperature = 0.58, beta = 0.2, lamda = 70.0, i_s = 50.0, omega = om, 
-#                                u1 = 0.5, u2 = -0.1, e = 0.0, i_0 = 88.0, omega_p = 0.0, raan = 0.0, t_p = 0.0, 
-#                                rp_rs = 0.1, obs_wavelength=800e-9, integration_grid_size=2)
+#                                u1 = 0.5, u2 = -0.1, e = 0.0, i_0 = 88.0, omega_p = 0.0, raan = 0.0, t_mid = 0.0, 
+#                                rp_rs = 0.1, obs_wavelength=800e-9, pl_grid=2)
+
 
 
 plt.figure(figsize=(8, 4))

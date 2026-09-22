ee05a59ee006adb2b70a60b9faf45be2327ef0e8
evoulimiotis <evoulimiotis2003@gmail.com>
2026-09-22 18:12:47 +0300

    Added new functions

--- ligrad/__init__.py
+++ ligrad/__init__.py
@@ -1,8 +1,5 @@
-from .main import grav_dark_transit_model, spheroid, rotation_matrix_x, rotation_matrix_y, rotation_matrix_z, \
-rotated_spheroid, vis_mask, ellipse_radius, gravity_darkening, limb_darkening, unocculted_flux, occulted_flux, kep2car, \
-vsini2omega
+from .main import gd_transit_model, transit_model, spheroid, rotation_matrix_x, rotation_matrix_y, rotation_matrix_z, \
+rotated_spheroid, vis_mask, ellipse_radius, gravity_darkening, limb_darkening, _unit_planet_grid, planet_grid, \
+sphere_unocculted_flux, unocculted_flux, sphere_intensity, intensity, _sphere_grid_flux, _grid_flux, sphere_occulted_flux, \
+occulted_flux, kep2car, vsini2omega
 
-# __all__ = ["grav_dark_transit_model","spheroid","rotation_matrix_x","rotation_matrix_y","rotation_matrix_z",
-#            "rotated_spheroid","vis_mask","ellipse_radius","gravity_darkening","limb_darkening","unocculted_flux",
-#            "occulted_flux","planet_position","vsini2omega"]
-# __version__ = "1.1.5"

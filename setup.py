b0ab83da1124eb3102bef58c8ee2af681602ccb2
evoulimiotis <evoulimiotis2003@gmail.com>
2026-09-22 18:12:19 +0300

    Bumped version to 1.2.0

--- setup.py
+++ setup.py
@@ -5,11 +5,10 @@ with open("README.md", "r") as f:
 
 setup(
     name='ligrad',
-    version='1.1.6',
+    version='1.2.0',
     packages=find_packages(),
     install_requires=[
         "numpy>=1.17",
-        "scipy>=1.5",
         "astropy>=4.0"
     ],
     

5faab45c5c46f6c7ad7ea3a4046d3ce42a292a9f
evoulimiotis <evoulimiotis2003@gmail.com>
2026-09-23 19:34:00 +0300

    Added package docstrings

--- ligrad/__init__.py
+++ ligrad/__init__.py
@@ -1,3 +1,14 @@
+"""
+# LiGrad
+
+A Python package for generating transit light-curves of gravity-darkened, oblate stars and more.
+
+## How to use the package
+
+Read the detailed documentation given in the GitHub [manual](https://github.com/evoulimiotis/ligrad)
+"""
+
+
 from .main import gd_transit_model, transit_model, spheroid, rotation_matrix_x, rotation_matrix_y, rotation_matrix_z, \
 rotated_spheroid, vis_mask, ellipse_radius, gravity_darkening, limb_darkening, _unit_planet_grid, planet_grid, \
 sphere_unocculted_flux, unocculted_flux, sphere_intensity, intensity, _sphere_grid_flux, _grid_flux, sphere_occulted_flux, \
